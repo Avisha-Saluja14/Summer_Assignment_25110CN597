8a5a8238a7fa1498900385224c02fc7743a0087c
Avisha-Saluja14 <avisha.saluja@gmail.com>
2026-06-01 12:00:00 +0530

    Day-1 uploaded

---
+++ Day-1/Q1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Q1
+{
+ public static void main(String []args)
+    {
+        int s=0,n,i;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number");
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            s=s+i;
+        }
+        System.out.println("Sum of natural numbers is="+s);
+
+    }
+    
+}
+ 
