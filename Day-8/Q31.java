bb324dc2d32ea163ba8dc87ea9c8fd76f1277835
Avisha-Saluja14 <avisha.saluja@gmail.com>
2026-06-08 21:13:24 +0530

    Day-8 uploaded

---
+++ Day-8/Q31.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Q31 
+{
+    public static void main(String[]args)
+    {
+        char i,j;
+        for(i='A';i<='E';i++)
+        {
+            for(j='A';j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
