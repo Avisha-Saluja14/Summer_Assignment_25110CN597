5611d9b6ad15061a6f5108c96674a8738f43373b
Avisha-Saluja14 <avisha.saluja@gmail.com>
2026-06-11 18:06:28 +0530

    Day-11 uploaded

---
+++ Day-11/Q42.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Q42 
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b,l;
+        System.out.println("Enter first number");
+        a=sc.nextInt();
+        System.out.println("Enter second number");
+        b=sc.nextInt();
+        l=max(a,b);
+        System.out.println("Maximum number is "+l);
+    }
+    static int max(int n,int m)
+    {
+        int l;
+        if(n>m)
+            l=n;
+        else
+            l=m;
+        return l;
+    }
+    
+}
